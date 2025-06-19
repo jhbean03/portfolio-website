083fcdfd8d8e11f2842bc405125db2adf4b4e2a2
Jackson <159603141+jhbean03@users.noreply.github.com>
2025-06-19 16:10:09 -0400

    Added burger menu and main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import Navbar from './components/Navbar.tsx'
 import { useState } from 'react'
+import portrait from './assets/jackson.jpg'
 
 const App = () => {
   const [theme, setTheme] = useState(true);
 
+  if (theme) {
+    document.getElementsByTagName("body")[0].className = "bg-slate-100"
+  } else {
+    document.getElementsByTagName("body")[0].className = "bg-slate-900"
+  }
+
   return (
-    <div className={theme ? "" : "dark"}>
+    <div className={`${theme ? "" : "dark"} text-[20px]`}>
       <Navbar theme={theme} setTheme={setTheme}/>
+      <div className="flex 2xl:flex-row flex-col gap-[2em] text-black dark:text-slate-200 bg-slate-200 dark:bg-slate-800 w-[70%] ml-auto mr-auto mt-[5em] rounded-xl p-[2em]">
+        <img src={portrait} className="h-[27rem] w-auto rounded-[15%] object-cover"/>
+        <div className="flex flex-col gap-[2em] p-[1em]">
+          <h1 className="font-[Offside] text-[40px] font-bold">Welcome!</h1>
+          <p>
+            Hi! My name is Jackson, and I am an Honors Computer Science & Engineering (CSE) and Theoretical Mathematics undegraduate at THE Ohio State University!
+            This is my portfolio website which contains more information about my professional goals, my personal interests, my skills, some of the projects I am
+            currently working on and have worked on in the past, and coursework I have completed. The latest version of my resume is included as well if you would
+            like to contact me with any questions regarding my work!
+          </p>
+        </div>
+      </div>
     </div>
+
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { FiMoon, FiSun } from "react-icons/fi";
+import { useState } from 'react'
 
 interface NavbarProps {
   theme: boolean
@@ -6,17 +7,20 @@ interface NavbarProps {
 }
 
 function Navbar({theme, setTheme}: NavbarProps) {
+  const [toggleNav, setToggleNav] = useState(true);
 
   return (
     <nav className="flex sticky top-[0] left-[0] z-1 items-center justify-between p-[1.5rem_3.5rem] m-[0] bg-slate-200 dark:bg-slate-800 shadow-[0_3px_5px_rgba(0,_0,_0,_0.3)]">
       <div className="font-[Offside] text-[40px] font-bold text-black dark:text-slate-200">JACKSON BEAN</div>
-      <ul className="flex items-center gap-[1.9rem] text-[20px] font-bold">
+      <ul id="nav-links" className={`flex items-center gap-[1.9rem] text-[20px] font-bold max-lg:absolute max-lg:right-0 max-lg:h-[42em] max-lg:top-[5.4em] 
+      max-lg:bg-slate-200 max-lg:dark:bg-slate-800 max-lg:flex-col max-lg:w-[35%] max-lg:transform-[translateX(${toggleNav ? "100%" : "0%" })] 
+      max-lg:transition-transform max-lg:duration-500 max-lg:ease-in`}>
         <li><a href="#about" className="duration-200 text-black dark:text-slate-200 hover:text-blue-600 dark:hover:text-blue-300">About Me</a></li>
         <li><a href="#skills" className="duration-200 text-black dark:text-slate-200 hover:text-blue-600 dark:hover:text-blue-300">Skills</a></li>
         <li><a href="#projects" className="duration-200 text-black dark:text-slate-200 hover:text-blue-600 dark:hover:text-blue-300">Projects</a></li>
         <li><a href="#coursework" className="duration-200 text-black dark:text-slate-200 hover:text-blue-600 dark:hover:text-blue-300">Coursework</a></li>
         <li><a href="#resume" className="duration-200 text-black dark:text-slate-200 hover:text-blue-600 dark:hover:text-blue-300">Resume</a></li>
-        <li className="">
+        <li>
           <button className="p-[0.75rem] bg-slate-300 dark:bg-slate-700 duration-200 hover:bg-slate-400 dark:hover:bg-slate-500 rounded-xl"
             onClick = { () => {
               setTheme(!theme);
@@ -26,6 +30,15 @@ function Navbar({theme, setTheme}: NavbarProps) {
           </button>
         </li>
       </ul>
+      <div className="hidden max-lg:block cursor-pointer"
+        onClick = { () => {
+          setToggleNav(!toggleNav);
+        }}
+      >
+        <div className="w-[25px] h-[3px] m-[5px] bg-black dark:bg-slate-200"></div>
+        <div className="w-[25px] h-[3px] m-[5px] bg-black dark:bg-slate-200"></div>
+        <div className="w-[25px] h-[3px] m-[5px] bg-black dark:bg-slate-200"></div>
+      </div>
     </nav>
   )
 }
