4963d5303e241634f7a708acccf08f8fe3ce651c
Jackson <159603141+jhbean03@users.noreply.github.com>
2025-06-27 18:04:44 -0400

    Finished styling the Projects page

--- src/components/ProjectBox.tsx
+++ src/components/ProjectBox.tsx
@@ -1,29 +1,41 @@
 interface ProjectBoxProps {
     picture: string;
     title: string;
+    date: string;
+    skills: string;
     description: string;
+    link: string;
     imgSide: string;
 }
 
-function ProjectBox({picture, title, description, imgSide}: ProjectBoxProps) {
+function ProjectBox({picture, title, date, skills, description, link, imgSide}: ProjectBoxProps) {
     return (
         <div>
-            <div className="grid grid-cols-2 grid-rows-[1fr_2fr] max-xl:hidden bg-slate-300 dark:bg-slate-700 p-[1em] rounded-4xl justify-items-center">
-                { imgSide == "left" ? <img src={picture} className="row-span-2 rounded-4xl"/> : "" }
-                <div>
+            <div className="grid grid-cols-2 gap-[1em] max-xl:hidden bg-slate-300 dark:bg-slate-700 p-[1em] rounded-4xl justify-items-center items-center">
+                { imgSide == "left" ? <img src={picture} className="row-span-2 rounded-4xl m-[2em] self-center"/> : "" }
+                <div className="pb-0">
                     <h2 className="font-[Offside] text-[24px] text-center font-bold p-0">{title}</h2>
-                    <div className="text-center"><i>May 2025</i></div>
+                    <div className="text-center"><i>{date}</i></div>
+                    <div className="text-center"><b>Relevant Skills:</b> {skills}</div>
+                    <div className="text-center mt-[1em]">
+                        <p className="mb-[1em]">{description}</p>
+                        { link == "" ? "" : <p><a href={link} target="_blank" className="duration-200 text-blue-600 hover:text-blue-400 dark:text-blue-300">Project Repository</a></p>}
+                    </div>
                 </div>
-                { imgSide == "right" ? <img src={picture} className="row-span-2 rounded-4xl"/> : "" }
-                <div>{description}</div>
+                { imgSide == "right" ? <img src={picture} className="rounded-4xl m-[2em] self-center"/> : "" }
             </div>
-            <div className="xl:hidden grid grid-cols-1 bg-slate-300 dark:bg-slate-700 p-[1em] rounded-4xl justify-items-center items-center gap-[1em]">
+
+            <div className="xl:hidden grid grid-cols-1 gap-[1em] bg-slate-300 dark:bg-slate-700 p-[1em] rounded-4xl justify-items-center items-center">
                 <img src={picture} className="row-span-2 rounded-4xl"/>
                 <div>
                     <h2 className="font-[Offside] text-[24px] text-center font-bold p-0">{title}</h2>
-                    <div className="text-center"><i>May 2025</i></div>
+                    <div className="text-center"><i>{date}</i></div>
+                    <div className="text-center"><b>Relevant Skills:</b> {skills}</div>
                 </div>
-                <div>{description}</div>
+                <div className="text-center">
+                    <p className="mb-[0.5em]">{description}</p>
+                    <p><a href={link} target="_blank" className="duration-200 text-blue-600 hover:text-blue-400 dark:text-blue-300">Project Repository</a></p>
+                </div>
             </div>
         </div>
     );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,19 @@
 import ProjectBox from "./ProjectBox"
+import redesignedPortfolioImg from "../assets/redesignedPortfolio.png"
 import apiImg from "../assets/apiImg.png"
+import travelImg from "../assets/travelImg.png"
 
 const PROJECTS = [
-    {picture: apiImg, title: "Full-Stack Employee Skill Search", description: "Something meaningful will go here eventually."},
-    {picture: apiImg, title: "Full-Stack Employee Skill Search", description: "Something meaningful will go here eventually."}
+    {picture: redesignedPortfolioImg, title: "Redesigned Portfolio", date: "June 2025 - Present", skills: "React, TypeScript, Tailwind CSS", 
+        description: "Starting in the summer of 2025, I began development of a new portfolio site that describes who I am as a person, what my career aspirations are"
+        + "and any relevant projects I have completed during my time studying computer science.", link: ""},
+    {picture: apiImg, title: "Full-Stack Employee Skill Search", date: "May 2025", skills: "React, TypeScript, APIs, MongoDB, Tailwind CSS", 
+        description: "Using Express.js and TypeScript, I constructed a backend with HTTPS routes to read and write information to a database I set up in MongoDB. "
+        + "With React and TypeScript, I was then able to create a dynamic and easy-to-use interface for employee skill searching.", link: "https://github.com/jhbean03/FullStackAPI"},
+    {picture: travelImg, title: "Travel Expense Calculator", date: "April 2025", skills: "Ruby on Rails, JavaScript, Sass", 
+        description: "For my web development projects class, me and a group of 4 others created a web application using Ruby on Rails which tracked various expenses made "
+        + "during multiple trips. The application was focused on how best to split up the expenses between any given number of travellers and stored user information securely "
+        + "within a database.", link: ""},
 ];
 
 const renderProjects = () => {
@@ -14,7 +24,8 @@ const renderProjects = () => {
         counter % 2 == 0 ? imgSide = "right" : imgSide = "left";
         counter++;
 
-        projectBoxElems.push(<ProjectBox picture={project.picture} title={project.title} description={project.description} imgSide={imgSide}/>);
+        projectBoxElems.push(<ProjectBox picture={project.picture} title={project.title} date={project.date} 
+            skills={project.skills} description={project.description} link={project.link} imgSide={imgSide}/>);
     });
     return projectBoxElems;
 }
